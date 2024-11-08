6a6b03adddf968cbed89d893f7ae392016c5dbf0
Hasnain <hacninetusar@gmail.com>
2024-11-08 10:02:05 +0600

    The issue:Navber stick to left side on learger screen is fixed.

--- src/components/Home/Header/Navbar.jsx
+++ src/components/Home/Header/Navbar.jsx
@@ -5,18 +5,20 @@ import { Link, useLocation } from 'react-router-dom';
 const Navbar = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     const location = useLocation();
+
     const toggleSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen);
     };
+
     return (
-        <div className="sticky top-0 w-full z-50">
-            <div className='bg-hero-bg wrapper absolute top-0 z-40 w-full '>
-                <navbar className="bg-transparent text-white wrapper">
-                    <div className="container mx-auto flex justify-between items-center ">
-                        <div className="center gap-10">
-                            <Link to={'/'} className="center gap-2">
+        <div className="sticky top-0 z-50 w-full   ">
+            <div className="max-w-[1368px] bg-hero-bg mx-auto w-full relative z-40">
+                <div className="bg-transparent text-white">
+                    <div className="container mx-auto flex justify-between items-center px-4 py-4">
+                        <div className="flex items-center gap-10">
+                            <Link to={'/'} className="flex items-center gap-2">
                                 <img src={"/sitelogo.svg"} alt="Site Logo" />
-                                <p className="text-4xl text-white font-poppins">Restaurant</p>
+                                <p className="text-4xl font-poppins">Restaurant</p>
                             </Link>
                             <nav className="hidden lg:flex space-x-6">
                                 <Link to={'/'} className={`border-b-2 ${location.pathname === '/' ? " border-yellow-400" : "text-white border-transparent"} hover:text-yellow-400 `}>Home</Link>
@@ -27,15 +29,14 @@ const Navbar = () => {
                                 <Link to={'/contact'} className={`border-b-2 ${location.pathname === '/contact' ? " border-yellow-400" : "text-white border-transparent"} hover:text-yellow-400 `}>Contact</Link>
                             </nav>
                         </div>
-                        <button className="bg-yellow-400 text-black px-4 py-2 xl:text-base text-sm font-poppins hidden font-semibold lg:block">
+                        <button className="bg-yellow-400 text-black px-4 py-2 xl:text-base text-sm font-poppins font-semibold hidden lg:block">
                             Book a Table
                         </button>
                         <button onClick={toggleSidebar} className="text-white text-2xl lg:hidden block">
-                            <img src="/icon/menu.svg" alt="" />
+                            <img src="/icon/menu.svg" alt="Menu Icon" />
                         </button>
                     </div>
-                </navbar>
-
+                </div>
 
                 <Sidebar isOpen={isSidebarOpen} onClose={toggleSidebar} />
             </div>
