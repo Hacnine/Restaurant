4030cb57bf7d5af744f3a8815b2e3c4283501709
Hasnain <hacninetusar@gmail.com>
2024-11-09 08:18:45 +0600

    Padding added.

--- src/components/Clients/Contact.jsx
+++ src/components/Clients/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <section className="text-center py-8 bg-customred text-white  shadow-md">
+        <section className="text-center py-8 px-4 bg-customred text-white  shadow-md">
             <h2 className="text-2xl md:text-3xl  mb-4">Interested in Working with Us?</h2>
             <p className="text-lg md:text-xl mb-6">
                 Contact us to plan your next unforgettable event with our exceptional dining services.
